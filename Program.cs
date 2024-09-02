77a105cb2f9c67dbaf82420033c16519df61e4e6
Daniel Hossaki <daniel.hossaki@outlook.com>
2024-09-02 15:12:59 -0300

    Resolução Desafio 3 LPOO - Celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+string opt = "";
+
+do
+{
+    Console.WriteLine("Escolha a marca: \n1 - Nokia \n2 - Iphone \n3 - Sair");
+    opt = Console.ReadLine();
+    if (opt == "1")
+    {
+        Nokia nokia = new Nokia("988887777", "tijolão", "CV2351FS", 3);
+        Console.WriteLine($"Número: {nokia.Numero} \nMarca: Nokia \nModelo: {nokia.Modelo}\nIMEI: {nokia.IMEI}\nMemória: {nokia.Memoria}");
+        nokia.InstalarAplicativo("Youtube");
+    }
+    else if (opt == "2")
+    {
+        Iphone iphone = new Iphone("944443333", "series s", "HJ5343Y", 128);
+        Console.WriteLine($"Número: {iphone.Numero} \nMarca: Iphone \nModelo: {iphone.Modelo}\nIMEI: {iphone.IMEI}\nMemória: {iphone.Memoria}");
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
+while (opt != "3");
+Console.WriteLine("Obrigado por utilizar nossa ferramenta!");
